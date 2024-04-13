17c1e7b4de54ebfccc8e8d19a3feb3185c1cdcf2
JoyB00 <dendypramartha7e@gmail.com>
2024-04-14 01:42:40 +0700

    authentication for customer done
    (cherry picked from commit a14fd3df5dce9a529a1509fb9cbd453f6ac0f1b7)

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
-Route::get('/register/verify/{verify_key}', [App\Http\Controllers\Api\AuthController::class, 'verify']);
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout',   [App\Http\Controllers\Api\AuthController::class, 'logout']);
+});
+
+
+// Route::get('/register/verify/{verify_key}', [App\Http\Controllers\Api\AuthController::class, 'verify']);
 
 Route::get('/produks', [App\Http\Controllers\Api\ProdukController::class, 'index']);
 Route::post('/produks', [App\Http\Controllers\Api\ProdukController::class, 'store']);
