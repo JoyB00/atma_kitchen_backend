63e1423d02359c2e6e4b70f5337627c02e115b85
bootloopmaster636 <christhartono@hotmail.com>
2024-04-15 11:52:05 +0700

    refactor: rename seeders

--- database/migrations/2024_04_12_103741_create_products_table.php
+++ database/migrations/2024_04_12_103741_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('consignor_id')->references('id')->on('consignors')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->foreignId('consignor_id')->references('id')->on('consignors')->onDelete('cascade')->onUpdate('cascade')->nullable()->change();
             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
             $table->string('product_name');
             $table->integer('quantity');
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -11,13 +11,13 @@ class RoleSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public static function run(): void
     {
         DB::table('roles')->insert([
-            ['id' => 1, 'nama_role' => 'Owner'],
-            ['id' => 2, 'nama_role' => 'Admin'],
-            ['id' => 3, 'nama_role' => 'MO'],
-            ['id' => 4, 'nama_role' => 'Customer'],
+            ['id' => 1, 'role_name' => 'Owner'],
+            ['id' => 2, 'role_name' => 'Admin'],
+            ['id' => 3, 'role_name' => 'MO'],
+            ['id' => 4, 'role_name' => 'Customer'],
         ]);
     }
 }
