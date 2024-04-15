b626f7cc79ee535be6aa9beb85652c7d20e85323
bootloopmaster636 <christhartono@hotmail.com>
2024-04-15 10:49:26 +0700

    fix: fix name too long on ingredient procurement details

--- database/migrations/2024_04_12_151447_create_ingredients_procurement_details_table.php
+++ database/migrations/2024_04_12_151447_create_ingredients_procurement_details_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ingredients_procurement_details', function (Blueprint $table) {
+        Schema::create('ingredients_prcmnt_dtl', function (Blueprint $table) { // ingredients_procurement_details
             $table->id();
             $table->foreignId('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('ingredient_procurements_id')->references('id')->on('ingredient_procurements')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('ingredient_prcmnt_id')->references('id')->on('ingredient_procurements')->onDelete('cascade')->onUpdate('cascade');
             $table->float('price');
             $table->integer('quantity');
             $table->float('total_price');
