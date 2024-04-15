5e6f79b261a511be1e8c98dd9b61c00810f0803b
bootloopmaster636 <christhartono@hotmail.com>
2024-04-15 10:18:08 +0700

    fix: migration error because name diff on cosignor(new) vs custodian

--- database/migrations/2024_04_12_103741_create_products_table.php
+++ database/migrations/2024_04_12_103741_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('custodian_id')->references('id')->on('custodians')->onDelete('cascade')->onUpdate('cascade')->nullable();
+            $table->foreignId('consignor_id')->references('id')->on('consignors')->onDelete('cascade')->onUpdate('cascade')->nullable();
             $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
             $table->string('product_name');
             $table->integer('quantity');
