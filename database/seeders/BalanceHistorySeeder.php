658921ef97fdde0c7478e5bb82dbd759930d9c57
bootloopmaster636 <christhartono@hotmail.com>
2024-04-23 20:45:34 +0700

    seeder: add carts

--- database/seeders/BalanceHistorySeeder.php
+++ database/seeders/BalanceHistorySeeder.php
@@ -6,37 +6,37 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class BalanceHistorySeeder extends Seeder
+class CartsSeeder extends Seeder
 {
   /**
    * Run the database seeds.
    */
   public static function run(): void
   {
-    DB::table('balance_histories')->insert(
+    DB::table('carts')->insert(
       [
-        'customer_id' => 1,
-        'balance_nominal' => 1000000,
-        'bank_name' => 'BCA',
-        'account_number' => '1234567890',
-        'date' => '2021-01-02',
-        'detail_information' => 'ksfjsjflksdfjkfdksl',
+        'transaction_id' => 1,
+        'product_id' => 1,
+        'hampers_id' => null,
+        'quantity' => 1,
+        'price' => 100000,
+        'total_price' => 100000,
       ],
       [
-        'customer_id' => 2,
-        'balance_nominal' => 0,
-        'bank_name' => 'BRI',
-        'account_number' => '1234567890',
-        'date' => '2021-01-01',
-        'detail_information' => 'Lorem ipsum',
+        'transaction_id' => 2,
+        'product_id' => null,
+        'hampers_id' => 1,
+        'quantity' => 2,
+        'price' => 200000,
+        'total_price' => 400000,
       ],
       [
-        'customer_id' => 3,
-        'balance_nominal' => 200000,
-        'bank_name' => 'Gopay',
-        'account_number' => '1234567890',
-        'date' => '2021-01-03',
-        'detail_information' => 'ksfjsjflksdfjkfdksl',
+        'transaction_id' => 3,
+        'product_id' => 2,
+        'hampers_id' => null,
+        'quantity' => 1,
+        'price' => 150000,
+        'total_price' => 150000,
       ]
     );
   }
