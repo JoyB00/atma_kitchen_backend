c0e76bd69ba409be5ae72121b2b7e96978326b46
bootloopmaster636 <christhartono@hotmail.com>
2024-04-23 21:15:03 +0700

    seeder: add hampers detail

--- app/Models/HampersDetails.php
+++ app/Models/HampersDetails.php
@@ -13,20 +13,23 @@ class HampersDetails extends Model
 
     protected $primaryKey = 'id';
     protected $fillable = [
-        'hampers_id', 
-        'product_id', 
+        'hampers_id',
+        'product_id',
         'ingredient_id'
     ];
 
-    public function Hampers(){
+    public function Hampers()
+    {
         return $this->belongsTo(Hampers::class, 'hampers_id');
     }
 
-    public function Product(){
+    public function Product()
+    {
         return $this->belongsTo(Product::class, 'product_id');
     }
 
-    public function Ingredients(){
+    public function Ingredients()
+    {
         return $this->belongsTo(Ingredients::class, 'ingredient_id');
     }
 }
