a206f6a42c49031f109075b363dc27b0312c2491
bootloopmaster636 <christhartono@hotmail.com>
2024-04-23 20:39:19 +0700

    seeder: add balance history

--- database/seeders/BalanceHistorySeeder.php
+++ database/seeders/BalanceHistorySeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class AttendanceSeeder extends Seeder
+class BalanceHistorySeeder extends Seeder
 {
   /**
    * Run the database seeds.
@@ -15,27 +15,29 @@ class AttendanceSeeder extends Seeder
   {
     DB::table('balance_histories')->insert(
       [
-        [
-          'employee_id' => 1,
-          'attendance_date' => '2021-01-02',
-          'is_bolos' => 1,
-        ],
-        [
-          'employee_id' => 1,
-          'attendance_date' => '2021-01-03',
-          'is_bolos' => 1,
-        ],
-        [
-          'employee_id' => 2,
-          'attendance_date' => '2021-01-01',
-          'is_bolos' => 1,
-        ],
-        [
-          'employee_id' => 2,
-          'attendance_date' => '2021-01-03',
-          'is_bolos' => 1,
-        ],
+        'customer_id' => 1,
+        'balance_nominal' => 1000000,
+        'bank_name' => 'BCA',
+        'account_number' => '1234567890',
+        'date' => '2021-01-02',
+        'detail_information' => 'ksfjsjflksdfjkfdksl',
       ],
+      [
+        'customer_id' => 2,
+        'balance_nominal' => 0,
+        'bank_name' => 'BRI',
+        'account_number' => '1234567890',
+        'date' => '2021-01-01',
+        'detail_information' => 'Lorem ipsum',
+      ],
+      [
+        'customer_id' => 3,
+        'balance_nominal' => 200000,
+        'bank_name' => 'Gopay',
+        'account_number' => '1234567890',
+        'date' => '2021-01-03',
+        'detail_information' => 'ksfjsjflksdfjkfdksl',
+      ]
     );
   }
 }
