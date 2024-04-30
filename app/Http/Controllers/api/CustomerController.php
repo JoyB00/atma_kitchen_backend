f7c1c278a52bd7aad9c0e5c27b27fba7d15599e2
bootloopmaster636 <christhartono@hotmail.com>
2024-04-30 21:12:40 +0700

    feat: add (experimental) customer controller

---
+++ app/Http/Controllers/api/CustomerController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+        $customer = Customers::find($id);
+        // $user = User::find($customer->user_id);
+        if (is_null($customer)) {
+            return response([
+                'message' => 'Customer Not Found'
+            ], 404);
+        }
+
+        //TODO need testing
+        $customer->update($request->all());
+        return response([
+            'message' => 'Customer Updated',
+            'data' => $customer
+        ], 200);
+    }
+
+    public function show($id)
+    {
+        $customer = Customers::find($id);
+        if (is_null($customer)) {
+            return response([
+                'message' => 'Customer Not Found'
+            ], 404);
+        }
+        return response([
+            'message' => 'Customer with ID ' . $id . ' Retrieved',
+            'data' => $customer
+        ], 200);
+    }
+}
