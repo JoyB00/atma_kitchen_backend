15860efb7e822585681cf63c3a3b439c859f2053
bootloopmaster636 <christhartono@hotmail.com>
2024-05-01 16:21:02 +0700

    feat: add delete to employees

--- app/Http/Controllers/api/EmployeeController.php
+++ app/Http/Controllers/api/EmployeeController.php
@@ -18,23 +18,20 @@ class EmployeeController extends Controller
 
         ], 200);
     }
-    public function store(Request $request)
+
+    public function update(Request $request, $id)
     {
-        $data = $request->all();
-        $user_id = auth()->user()->id;
-        $validate = Validator::make($data, [
-            'word_start_date' => 'required',
-        ]);
-        if ($validate->fails()) {
+        $employee = Employees::find($id);
+        if (is_null($employee)) {
             return response([
-                'message' => $validate->errors()->first(),
-            ], 400);
+                'message' => 'Employee Not Found'
+            ], 404);
         }
-        $data['user_id'] = $user_id;
-        $employee = Employees::create($data);
+
+        $employee->update($request->all());
         return response([
-            'message' => "Employee Created Successfully",
-            'data' => $employee,
+            'message' => 'Employee Updated',
+            'data' => $employee
         ], 200);
     }
 
@@ -49,6 +46,20 @@ class EmployeeController extends Controller
         return response([
             'message' => 'A Employee Retrieved',
             'data' => $employee
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        $employee = Employees::find($id);
+        if (is_null($employee)) {
+            return response([
+                'message' => 'Employee Not Found'
+            ], 404);
+        }
+        $employee->delete();
+        return response([
+            'message' => 'Employee Deleted'
         ], 200);
     }
 }
