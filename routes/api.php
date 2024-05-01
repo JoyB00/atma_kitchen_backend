52f22bebe6b27ed1846255f639ebb81fd27038f9
bootloopmaster636 <christhartono@hotmail.com>
2024-05-01 13:21:26 +0700

    fix: route/class not found error on Linux

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,34 @@
 <?php
 
+namespace App\Http\Controllers\api;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
-Route::post('/employee', [App\Http\Controllers\Api\AuthController::class, 'employeeRegister']);
-Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/employee', [AuthController::class, 'employeeRegister']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
-    Route::post('/logout',   [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::post('/logout',   [AuthController::class, 'logout']);
 });
 
 
 // Route::get('/register/verify/{verify_key}', [App\Http\Controllers\Api\AuthController::class, 'verify']);
 
-Route::get('/product', [App\Http\Controllers\Api\ProductController::class, 'index']);
-Route::post('/product', [App\Http\Controllers\Api\ProductController::class, 'store']);
-Route::post('/product/{id}', [App\Http\Controllers\Api\ProductController::class, 'update']);
-Route::delete('/product/{id}', [App\Http\Controllers\Api\ProductController::class, 'destroy']);
-Route::get('/product/{id}', [App\Http\Controllers\Api\ProductController::class, 'getProduct']);
-Route::get('/category', [App\Http\Controllers\Api\CategoryController::class, 'index']);
-Route::delete('/category/{id}', [App\Http\Controllers\Api\CategoryController::class, 'destroy']);
-Route::get('/ingredient', [App\Http\Controllers\Api\IngredientController::class, 'index']);
-Route::get('/hampers', [App\Http\Controllers\Api\HampersController::class, 'index']);
-Route::post('/hampers', [App\Http\Controllers\Api\HampersController::class, 'store']);
-Route::delete('/hampers/{id}', [App\Http\Controllers\Api\HampersController::class, 'destroy']);
-Route::get('/hampers/{id}', [App\Http\Controllers\Api\HampersController::class, 'getHampers']);
-Route::post('/hampers/{id}', [App\Http\Controllers\Api\HampersController::class, 'update']);
-Route::get('/consignor', [App\Http\Controllers\Api\ConsignorController::class, 'index']);
-Route::get('/ingredientProcurement', [App\Http\Controllers\Api\IngredientProcurementController::class, 'index']);
-Route::get('/ingredientProcurement/{id}', [App\Http\Controllers\Api\IngredientProcurementController::class, 'getIngredientProcurement']);
+Route::get('/product', [ProductController::class, 'index']);
+Route::post('/product', [ProductController::class, 'store']);
+Route::post('/product/{id}', [ProductController::class, 'update']);
+Route::delete('/product/{id}', [ProductController::class, 'destroy']);
+Route::get('/product/{id}', [ProductController::class, 'getProduct']);
+Route::get('/category', [CategoryController::class, 'index']);
+Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+Route::get('/ingredient', [IngredientController::class, 'index']);
+Route::get('/hampers', [HampersController::class, 'index']);
+Route::post('/hampers', [HampersController::class, 'store']);
+Route::delete('/hampers/{id}', [HampersController::class, 'destroy']);
+Route::get('/hampers/{id}', [HampersController::class, 'getHampers']);
+Route::post('/hampers/{id}', [HampersController::class, 'update']);
+Route::get('/consignor', [ConsignorController::class, 'index']);
+Route::get('/ingredientProcurement', [IngredientProcurementController::class, 'index']);
+Route::get('/ingredientProcurement/{id}', [IngredientProcurementController::class, 'getIngredientProcurement']);
