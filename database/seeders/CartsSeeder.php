d94efe8132db86e5f92e4eb3d61079b7f7af0edc
bootloopmaster636 <christhartono@hotmail.com>
2024-05-03 22:37:03 +0700

    fix: error during seeding transaction

--- database/seeders/CartsSeeder.php
+++ database/seeders/CartsSeeder.php
@@ -15,37 +15,40 @@ class CartsSeeder extends Seeder
   {
     DB::table('carts')->insert(
       [
-        "transaction_id" => 1,
-        "product_id" => 1,
-        "hampers_id" => null,
-        "quantity" => 1,
-        "price" => 850000,
-        "total_price" => 850000
-      ],
-      [
-        "transaction_id" => 2,
-        "product_id" => 2,
-        "hampers_id" => null,
-        "quantity" => 1,
-        "price" => 450000,
-        "total_price" => 450000
-      ],
-      [
-        "transaction_id" => 3,
-        "product_id" => 3,
-        "hampers_id" => null,
-        "quantity" => 1,
-        "price" => 550000,
-        "total_price" => 550000
-      ],
-      [
-        "transaction_id" => 4,
-        "product_id" => 4,
-        "hampers_id" => null,
-        "quantity" => 1,
-        "price" => 300000,
-        "total_price" => 300000
-      ],
+        [
+          "transaction_id" => 1,
+          "product_id" => 1,
+          "hampers_id" => null,
+          "quantity" => 1,
+          "price" => 850000,
+          "total_price" => 850000
+        ],
+        [
+          "transaction_id" => 2,
+          "product_id" => 2,
+          "hampers_id" => null,
+          "quantity" => 1,
+          "price" => 450000,
+          "total_price" => 450000
+        ],
+        [
+          "transaction_id" => 3,
+          "product_id" => 3,
+          "hampers_id" => null,
+          "quantity" => 1,
+          "price" => 550000,
+          "total_price" => 550000
+        ],
+        [
+          "transaction_id" => 4,
+          "product_id" => 4,
+          "hampers_id" => null,
+          "quantity" => 1,
+          "price" => 300000,
+          "total_price" => 300000
+        ],
+      ]
+
     );
   }
 }
