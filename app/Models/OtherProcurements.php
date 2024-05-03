e0bb4a468db875f13a0ae371d67e45eacb8fc71a
JoyB00 <dendypramartha7e@gmail.com>
2024-05-03 16:30:38 +0700

    make otherProcurement seeder

--- app/Models/OtherProcurements.php
+++ app/Models/OtherProcurements.php
@@ -9,17 +9,18 @@ class OtherProcurement extends Model
 {
     use HasFactory;
     protected $table = 'other_procurements';
-    
+
     protected $primaryKey = 'id';
     protected $fillable = [
-        'employees_id', 
-        'ingredient_name', 
-        'price', 
-        'quantity', 'procurement_date', 
+        'employee_id',
+        'ingredient_name',
+        'price',
+        'quantity', 'procurement_date',
         'total_price'
     ];
 
-    public function Employees(){
+    public function Employees()
+    {
         return $this->belongsTo(Employees::class, 'employee_id');
     }
 }
--- database/seeders/OtherProcurementSeeder.php
+++ database/seeders/OtherProcurementSeeder.php
@@ -15,45 +15,12 @@ class OtherProcurementSeeder extends Seeder
   {
     DB::table('other_procurements')->insert(
       [
-        'employee_id' => 1,
-        'ingredient_name' => 'Listrik',
-        'price' => 3890000,
-        'quantity' => 1,
-        'procurement_date' => '2024-02-08',
-        'total_price' => 3890000,
-      ],
-      [
-        'employee_id' => 1,
-        'ingredient_name' => 'Iuran RT',
-        'price' => 500000,
-        'quantity' => 1,
-        'procurement_date' => '2024-02-08',
-        'total_price' => 500000,
-      ],
-      [
-        'employee_id' => 1,
-        'ingredient_name' => 'bensin',
-        'price' => 900000,
-        'quantity' => 1,
-        'procurement_date' => '2024-02-08',
-        'total_price' => 900000,
-      ],
-      [
-        'employee_id' => 1,
-        'ingredient_name' => 'Gas',
-        'price' => 2200000,
-        'quantity' => 1,
-        'procurement_date' => '2024-02-08',
-        'total_price' => 2200000,
-      ],
-      [
-        'employee_id' => 3,
-        'ingredient_name' => 'Rapat Pegawai',
-        'price' => 250000,
-        'quantity' => 1,
-        'procurement_date' => '2024-02-08',
-        'total_price' => 250000,
-      ],
+        ['id' => 1, 'employee_id' => 1, 'ingredient_name' => 'Listrik', 'price' => 3890000, 'quantity' => 1, 'procurement_date' => '2024-02-05', 'total_price' => 389000],
+        ['id' => 2, 'employee_id' => 1, 'ingredient_name' => 'Iuran RT', 'price' => 500000, 'quantity' => 1, 'procurement_date' => '2024-02-08', 'total_price' => 500000],
+        ['id' => 3, 'employee_id' => 1, 'ingredient_name' => 'Bensin', 'price' => 900000, 'quantity' => 1, 'procurement_date' => '2024-01-08', 'total_price' => 900000],
+        ['id' => 4, 'employee_id' => 1, 'ingredient_name' => 'Gas', 'price' => 2200000, 'quantity' => 1, 'procurement_date' => '2024-02-08', 'total_price' => 2200000],
+        ['id' => 5, 'employee_id' => 1, 'ingredient_name' => 'Rapat Pegawai', 'price' => 250000, 'quantity' => 1, 'procurement_date' => '2024-02-08', 'total_price' => 250000]
+      ]
     );
   }
 }
