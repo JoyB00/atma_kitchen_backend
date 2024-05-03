39bd394be70c43451ab9e2c6eb0d411837365c5d
JoyB00 <dendypramartha7e@gmail.com>
2024-05-03 13:01:29 +0700

    edit: error handling validate store and update consignor ver 2

--- app/Http/Controllers/api/ConsignorController.php
+++ app/Http/Controllers/api/ConsignorController.php
@@ -48,7 +48,7 @@ class ConsignorController extends Controller
         $storeData = $request->all();
         $validate = Validator::make($storeData, [
             'consignor_name' => 'required',
-            'phone_number' => 'required|numeric|min:11|max:13'
+            'phone_number' => 'required|numeric|min:11'
         ]);
         if ($validate->fails()) {
             return response([
@@ -75,7 +75,7 @@ class ConsignorController extends Controller
         $updateData = $request->all();
         $validate = Validator::make($updateData, [
             'consignor_name' => 'required',
-            'phone_number' => 'required|numeric|min:11|max:13'
+            'phone_number' => 'required|numeric|min:11'
         ]);
         if ($validate->fails()) {
             return response([
