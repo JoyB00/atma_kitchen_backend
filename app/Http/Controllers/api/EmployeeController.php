602863caece1aedbf8c2631308f3a9b33c2add62
bootloopmaster636 <christhartono@hotmail.com>
2024-05-04 21:37:53 +0700

    fix: now showing only active=1 in employee Co-authored-by: JoyB00 <dendypramartha03@gmail.com>

--- app/Http/Controllers/api/EmployeeController.php
+++ app/Http/Controllers/api/EmployeeController.php
@@ -12,7 +12,9 @@ class EmployeeController extends Controller
 {
     public function index()
     {
-        $employee = Employees::with('Users')->where('users.active', 1)->get();
+        $employee = Employees::with('Users')->whereHas('Users', function ($query) {
+            $query->where('active', 1);
+        })->get();
         return response([
             'message' => "Retrieve All Employee Successfully",
             'data' => $employee,
