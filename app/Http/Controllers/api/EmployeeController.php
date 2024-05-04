30d4815e27b411eb3fdf32fe611ce5cee4f9c56b
bootloopmaster636 <christhartono@hotmail.com>
2024-05-04 21:37:53 +0700

    Co-authored-by: JoyB00 <dendypramartha03@gmail.com>

--- app/Http/Controllers/api/EmployeeController.php
+++ app/Http/Controllers/api/EmployeeController.php
@@ -12,7 +12,7 @@ class EmployeeController extends Controller
 {
     public function index()
     {
-        $employee = Employees::with('Users')->where('active', 1)->get();
+        $employee = Employees::with('Users')->where('Users.active', 1)->get();
         return response([
             'message' => "Retrieve All Employee Successfully",
             'data' => $employee,
