35c9d37cf8b29aefd3dc4d1f90d142e7d2fb37c1
JoyB00 <dendypramartha7e@gmail.com>
2024-05-05 16:29:25 +0700

    edit attendance seeder

--- database/seeders/AttendancesSeeder.php
+++ database/seeders/AttendancesSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class AttendanceSeeder extends Seeder
+class AttendancesSeeder extends Seeder
 {
   /**
    * Run the database seeds.
