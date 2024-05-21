54d98fd2532df17580af320c0071672a6e612d35
JoyB00 <dendypramartha7e@gmail.com>
2024-05-21 22:19:19 +0700

    handling order date ver 5

--- app/Http/Controllers/api/CartsController.php
+++ app/Http/Controllers/api/CartsController.php
@@ -59,8 +59,8 @@ class CartsController extends Controller
         $validate->after(function ($validator) use ($storeData) {
             if (!is_null($storeData['product_id'])) {
                 $exists = DB::table('carts')
-                    ->where('product_id', $storeData['product_id'])
-                    ->where('order_date', Carbon::parse($storeData['order_date'])->toDateString())
+                    ->where('product_id', 49)
+                    ->whereDate('order_date', Carbon::parse($storeData['order_date'])->toDateString())
                     ->exists();
                 if ($exists) {
                     $validator->errors()->add('product_id', 'The Product with this order date is already in your cart');
