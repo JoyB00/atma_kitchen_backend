bea4406335a4d90a12b311cced0ed03821b7acf7
JoyB00 <dendypramartha7e@gmail.com>
2024-05-24 02:48:27 +0700

    update getSnapToken ver 10

--- app/Http/Controllers/api/PaymentController.php
+++ app/Http/Controllers/api/PaymentController.php
@@ -16,33 +16,32 @@ class PaymentController extends Controller
         Config::$isSanitized = true;
         Config::$is3ds = true;
 
-        $details = $request->input('details', []);
+        // $details = $request->input('details', []);
 
-        $items = [];
-        foreach ($details as $item) {
-            if (is_null($item['hampers_id'])) {
-                $items[] = [
-                    "id" => $item['product']['id'],
-                    "price" => 5000,
-                    "quantity" => $item['quantity'],
-                    "name" => $item['product']['product_name'],
-                ];
-            } else if (is_null($item['product_id'])) {
-                $items[] = [
-                    "id" => $item['hampers']['id'],
-                    "price" => 5000,
-                    "quantity" => $item['quantity'],
-                    "name" => $item['hampers']['hampers_name'],
-                ];
-            }
-        }
+        // $items = [];
+        // foreach ($details as $item) {
+        //     if (is_null($item['hampers_id'])) {
+        //         $items[] = [
+        //             "id" => $item['product']['id'],
+        //             "price" => $item['product']['product_price'],
+        //             "quantity" => $item['quantity'],
+        //             "name" => $item['product']['product_name'],
+        //         ];
+        //     } else if (is_null($item['product_id'])) {
+        //         $items[] = [
+        //             "id" => $item['hampers']['id'],
+        //             "price" => $item['hampers']['hampers_price'],
+        //             "quantity" => $item['quantity'],
+        //             "name" => $item['hampers']['hampers_name'],
+        //         ];
+        //     }
+        // }
 
         $params = [
             'transaction_details' => [
                 'order_id' => uniqid(),
                 'gross_amount' => $request->amount,
             ],
-            "item_details" => $items,
             'customer_details' => [
                 'first_name' => $request->first_name,
                 'last_name' => $request->last_name,
@@ -53,7 +52,7 @@ class PaymentController extends Controller
 
         try {
             $snapToken = Snap::getSnapToken($params);
-            return response(['snapToken' => $snapToken, $items]);
+            return response(['snapToken' => $snapToken]);
         } catch (\Exception $e) {
             return response(['error' => $e->getMessage()], 500);
         }
