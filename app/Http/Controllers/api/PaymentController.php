a74917370d1a87b95e122a5b98978da03a0e38ca
JoyB00 <dendypramartha7e@gmail.com>
2024-05-24 02:07:42 +0700

    add item detail

--- app/Http/Controllers/api/PaymentController.php
+++ app/Http/Controllers/api/PaymentController.php
@@ -16,11 +16,31 @@ class PaymentController extends Controller
         Config::$isSanitized = true;
         Config::$is3ds = true;
 
+        $details = $request->input('details');
+
+        $items = [];
+        foreach ($details as $item) {
+            if (is_null($item['hampers_id'])) {
+                $items[] = [
+                    "price" => $item['product']->price,
+                    "quantity" => $item['quantity'],
+                    "name" => $item['product']->product_name,
+                ];
+            } else if (is_null($item['product_id'])) {
+                $items[] = [
+                    "price" => $item['hampers']->price,
+                    "quantity" => $item['quantity'],
+                    "name" => $item['hampers']->hampers_name,
+                ];
+            }
+        }
+
         $params = [
             'transaction_details' => [
                 'order_id' => uniqid(),
                 'gross_amount' => $request->amount,
             ],
+            "item_detail" => $items,
             'customer_details' => [
                 'first_name' => $request->first_name,
                 'last_name' => $request->last_name,
