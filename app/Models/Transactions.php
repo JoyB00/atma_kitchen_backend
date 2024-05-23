efaa0c06867a24df00e8a80de8b128e28b1c56fc
JoyB00 <dendypramartha7e@gmail.com>
2024-05-23 14:36:11 +0700

    update orderDetailAuth

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -10,33 +10,35 @@ class Transactions extends Model
     use HasFactory;
     protected $table = 'transactions';
 
-   protected $primaryKey = 'id';
-   protected $fillable = [
-    'employee_id', 
-    'customer_id', 
-    'delivery_id', 
-    'order_date', 
-    'paidoff_date', 
-    'pickup_date', 
-    'payment_method', 
-    'status', 
-    'payment_evidence', 
-    'used_point', 
-    'earned_point', 
-    'total_price', 
-    'tip'
-   ];
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'employee_id',
+        'customer_id',
+        'delivery_id',
+        'order_date',
+        'paidoff_date',
+        'pickup_date',
+        'payment_method',
+        'status',
+        'payment_evidence',
+        'used_point',
+        'earned_point',
+        'total_price',
+        'tip'
+    ];
 
-   public function Employees(){
-    return $this->belongsTo(Employees::class, 'employee_id');
-   }
+    public function Employees()
+    {
+        return $this->belongsTo(Employees::class, 'employee_id');
+    }
 
-   public function Customers(){
-    return $this->belongsTo(Customers::class, 'customer_id');
-   }
+    public function Customers()
+    {
+        return $this->belongsTo(Customers::class, 'customer_id');
+    }
 
-   public function Deliveries(){
-    return $this->belongsTo(Deliveries::class, 'delivery_id');
-   }
-   
+    public function Deliveries()
+    {
+        return $this->belongsTo(Deliveries::class, 'delivery_id');
+    }
 }
