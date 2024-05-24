c2a9ff5de8212d9b7f6ef369a97836af0d1e1092
bootloopmaster636 <christhartono@hotmail.com>
2024-05-24 21:18:43 +0700

    fix: delivery distance 2

--- app/Http/Controllers/api/DeliveryDistanceController.php
+++ app/Http/Controllers/api/DeliveryDistanceController.php
@@ -3,35 +3,45 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Deliveries;
-use App\Models\Transactions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class DeliveryDistanceController extends Controller
 {
     public function store(Request $request)
     {
         $data = $request->all();
-        $delivery = Deliveries::create($data);
-        $transaction = Transactions::find($data['transaction_id']);
-        $transaction->delivery_id = $delivery->id;
-        $transaction->save();
+        $validate = Validator::make(
+            $data,
+            [
+                'id' => 'required',
+                'distance' => 'required',
+            ],
+        );
+        if ($validate->fails()) {
+            return response([
+                'message' => $validate->errors()->first()
+            ], 400);
+        }
+
+        // set shipping cost
+        $data['shipping_cost'] = 0;
 
         return response([
-            'message' => 'The delivery has been successfully added.',
+            'message' => 'The delivery distance has been successfully added.',
             'data' => $delivery
         ], 200);
     }
 
     public function show()
     {
-//        $deliveries = Transactions::with('Delivery')->whereHas('Delivery', function ($query) {
-//            $query->where('distance', '==', null)->where('delivery_method', '==', 'Delivery Courier');
-//        })->get();
-        $deliveries = Deliveries::where('distance', null)->where('delivery_method', 'Delivery Courier')->get();
+        $deliveries = Transactions::with('Delivery')->whereHas('Delivery', function ($query) {
+            $query->where('distance', '==', null)->where('delivery_method', '==', 'Delivery Courier');
+        })->get();
+//        $deliveries = Deliveries::where('distance', null)->where('delivery_method', 'Delivery Courier')->get();
 
         return response([
-            'message' => 'All data Retrievied',
+            'message' => 'All data Retrieved',
             'data' => $deliveries,
         ], 200);
     }
