d9c2da10b84998a218cf5a024e4b5871c79afeb7
JoyB00 <dendypramartha7e@gmail.com>
2024-05-30 21:24:18 +0700

    update recapUsedIngredient ver 5

--- app/Http/Controllers/api/TransactionConfirmationController.php
+++ app/Http/Controllers/api/TransactionConfirmationController.php
@@ -158,7 +158,7 @@ class TransactionConfirmationController extends Controller
         $transaksiID_array = [];
 
         foreach ($data['item'] as $key => $i) {
-            $temp = Transactions::with('Customer', 'Customer.Users', 'TransactionDetails', 'TramsactionDetails.Product', 'TramsactionDetails.Hampers', 'TramsactionDetails.Product.AllRecipes', 'TramsactionDetails.Product.AllRecipes.Ingredients')->find($i['id']);
+            $temp = Transactions::with('Customer', 'Customer.Users', 'TransactionDetails', 'TransactionDetails.Product', 'TransactionDetails.Hampers', 'TransactionDetails.Product.AllRecipes', 'TransactionDetails.Product.AllRecipes.Ingredients')->find($i['id']);
             $transaksiArray[$key] = $temp;
             $transaksiID_array[$key] = $temp->id;
         }
