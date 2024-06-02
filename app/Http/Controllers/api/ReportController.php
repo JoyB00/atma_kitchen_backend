b74d1ea0d0a4e7b8fb93e0afb87fb3a855d3ad6e
JoyB00 <dendypramartha7e@gmail.com>
2024-06-02 22:42:51 +0700

    update getProductSalesByMonth ver 6

--- app/Http/Controllers/api/ReportController.php
+++ app/Http/Controllers/api/ReportController.php
@@ -16,12 +16,14 @@ class ReportController extends Controller
         $products = TransactionDetail::join('transactions', 'transaction_details.transaction_id', '=', 'transactions.id')
             ->leftJoin('products', 'transaction_details.product_id', '=', 'products.id')
             ->leftJoin('hampers', 'transaction_details.hampers_id', '=', 'hampers.id')
-            ->selectRaw('COALESCE(products.product_name, hampers.hampers_name) as Product')
-            ->selectRaw('SUM(transaction_details.quantity) as Quantity')
-            ->select('transaction_details.price as Price')
-            ->selectRaw('SUM(transaction_details.quantity * transaction_details.price) as total_harga')
-            ->whereMonth('transactions.pickup_date', '=', $month)
+            ->select(
+                DB::raw('COALESCE(products.product_name, hampers.hampers_name) as Product'),
+                'transaction_details.quantity as Quantity',
+                'transaction_details.price as Price'
+            )
+            ->whereMonth('transactions.pickup_date', '=',  $month)
             ->groupBy('Product', 'Price', 'Quantity')
+            ->orderBy('Product')
             ->get();
 
         return response([
