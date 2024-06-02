d5d8c7252635c76ee680c2247f5c96f93b0b9519
JoyB00 <dendypramartha7e@gmail.com>
2024-06-02 22:26:02 +0700

    add getProductSalesByMonth

--- app/Http/Controllers/api/ReportController.php
+++ app/Http/Controllers/api/ReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,18 +13,15 @@ class ReportController extends Controller
     {
         $pickupDate = $request->input('pickup_date');
 
-        $products = DB::table('transactions')
-            ->join('transaction_details', 'transactions.id', '=', 'transaction_details.transaction_id')
-            ->join('products', 'transaction_details.product_id', '=', 'products.id')
-            ->select('products.product_name as Product', 'transaction_details.quantity as Quantity', 'transaction_details.price as Price')
-            ->whereMonth('transactions.pickup_date', '=', $pickupDate)
-            ->unionAll(
-                DB::table('transactions')
-                    ->join('transaction_details', 'transactions.id', '=', 'transaction_details.transaction_id')
-                    ->join('hampers', 'transaction_details.hampers_id', '=', 'hampers.id')
-                    ->select('hampers.hampers_name as Product', 'transaction_details.quantity as Quantity', 'transaction_details.price as Price')
-                    ->whereMonth('transactions.pickup_date', '=', $pickupDate)
+        $products = TransactionDetail::join('transactions', 'transaction_details.transaction_id', '=', 'transactions.id')
+            ->leftJoin('products', 'transaction_details.product_id', '=', 'products.id')
+            ->leftJoin('hampers', 'transaction_details.hampers_id', '=', 'hampers.id')
+            ->select(
+                DB::raw('COALESCE(products.product_name, hampers.hampers_name) as Product'),
+                'transaction_details.quantity as Quantity',
+                'transaction_details.price as Price'
             )
+            ->whereMonth('transactions.pickup_date', '=',  $pickupDate)
             ->groupBy('Product', 'Price')
             ->orderBy('Product')
             ->get();
